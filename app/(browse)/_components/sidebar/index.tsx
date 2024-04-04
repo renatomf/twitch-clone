8b2b2acbc4ebc2633fdaa6e3ec4d7f381a0d84bc
renatomf <renatomar76@gmail.com>
2024-04-04 16:35:46 -0300

    follow list

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,21 +1,21 @@
-//import { getFollowedUsers } from "@/lib/follow-service";
+import { getFollowedUsers } from "@/lib/follow-service";
 import { getRecommended } from "@/lib/recommended-service";
 
 import { Wrapper } from "./wrapper";
-//import { Following, FollowingSkeleton } from "./following";
+import { Following, FollowingSkeleton } from "./following";
 import { Toggle, ToggleSkeleton } from "./toggle";
 import { Recommended, RecommendedSkeleton } from "./recommended";
 
 export const Sidebar = async () => {
   const recommended = await getRecommended();
-  //const following = await getFollowedUsers();
+  const following = await getFollowedUsers();
 
   return (
     <Wrapper>
       <Toggle />
       <div className="space-y-4 pt-4 lg:pt-0">
+        <Following data={following} />
         <Recommended data={recommended} />
-        {/*<Following data={following} />*/}
       </div>
     </Wrapper>
   );
@@ -25,8 +25,8 @@ export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full border-r border-[#2D2E35] z-50">
       <ToggleSkeleton />
+      <FollowingSkeleton />
       <RecommendedSkeleton />
-      {/*<FollowingSkeleton />*/}
     </aside>
   );
 };
--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -7,7 +7,7 @@ import { useSidebar } from "@/store/use-sidebar";
 
 import { ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton } from "./recommended";
-//import { FollowingSkeleton } from "./following";
+import { FollowingSkeleton } from "./following";
 
 interface WrapperProps {
   children: React.ReactNode;
@@ -23,8 +23,8 @@ export const Wrapper = ({
     return (
       <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full border-r border-[#2D2E35] z-50">
         <ToggleSkeleton />
+        <FollowingSkeleton />
         <RecommendedSkeleton />
-        {/*<FollowingSkeleton />*/}
       </aside>
     );
   }
